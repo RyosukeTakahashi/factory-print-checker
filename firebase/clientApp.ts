19a1cf221dd1a9183b2dc97a1ae6c1d7301db86f
Ryo Murakami <ryo.a.murakami@dena.com>
2020-11-24 21:22:24 +0900

    remove analytics

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -2,7 +2,6 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
-import "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -17,9 +16,6 @@ const firebaseConfig = {
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
-  if (typeof window !== "undefined") {
-    firebase.analytics();
-  }
 }
 
 export default firebase;
