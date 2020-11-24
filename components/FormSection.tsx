bab7ece03da614f871657b5f37e74b6157d6dda8
Ryo Murakami <ryo.a.murakami@dena.com>
2020-11-24 19:35:32 +0900

    delete files

--- components/FormSection.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ReactNode } from "react";
-
-export const FormSection = (props: { children: ReactNode }) => (
-  <div className="mt-4 px-3 py-4 bg-white rounded-lg">{props.children}</div>
-);
--- components/FormSectionTitle.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const FormSectionTitle = (props: { title: string }) => (
-  <div className="text-xl mb-3">
-    <p>{props.title}</p>
-  </div>
-);
